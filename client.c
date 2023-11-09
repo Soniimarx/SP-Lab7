0ee47488130873d73950a94bc4998057abfabeb7
Shemar Marks <49130526+Soniimarx@users.noreply.github.com>
2023-11-09 12:16:13 -0500

    Added the client and server files.
    Added the client and server files that allow the relationship/joint application to work.

---
+++ client.c
@@ -0,0 +1,67 @@
+/* Single server-client connection. Small modifications by C. Busby-Earle */
+// Shemar Marks : 620149911
+
+
+//client.c
+#include <stdio.h>
+#include <stdlib.h> /* for exit () function */
+#include <string.h> /* for memset() and string functions */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> /* for inet_addr() function */
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+#define SERVER_IP "127.0.0.1" /* change this to your server's address */
+#define SERVER_PORT 60000     /* change this to your server's port */
+
+int main(int argc, char *argv[])
+
+{
+  int sock_send;
+  struct sockaddr_in addr_send;
+  int i;
+  char text[80], buf[BUF_SIZE];
+  int send_len, bytes_sent;
+
+  /* create socket for sending data */
+  sock_send = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sock_send < 0)
+  {
+    printf("socket() failed\n");
+    exit(0);
+  }
+
+  /* create socket address structure to connect to */
+  memset(&addr_send, 0, sizeof(addr_send)); /* zero out structure */
+  addr_send.sin_family = AF_INET;           /* address family */
+  addr_send.sin_addr.s_addr = inet_addr(SERVER_IP);
+  addr_send.sin_port = htons((unsigned short)SERVER_PORT);
+
+  /* connect to the server */
+  i = connect(sock_send, (struct sockaddr *)&addr_send, sizeof(addr_send));
+  if (i < 0)
+  {
+    printf("connect() failed\n");
+    exit(0);
+  }
+
+  while (1)
+  {
+    /* send some data */
+    printf("Send? ");
+    scanf("%s", text);
+    if (strcmp(text, "quit") == 0)
+      break;
+
+    strcpy(buf, text);
+    send_len = strlen(text);
+    bytes_sent = send(sock_send, buf, send_len, 0);
+  }
+
+  close(sock_send);
+}
+
+/**/
